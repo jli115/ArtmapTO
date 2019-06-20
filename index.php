3fe3536c326989280f302889e0bc0aedd918db69
jli115 <jli115@syr.edu>
2019-06-20 14:39:48 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  <? include "head.php" ?>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    </body>
+</html>
